c352b9041a1cdcf720d60f20f85554548de98196
bortiskezikov <boris200898@icloud.com>
2020-05-13 00:41:32 +0300

    remote kafka fix

--- src/main/java/com/greenapp/mailservice/kafka/MailTopics.java
+++ src/main/java/com/greenapp/mailservice/kafka/MailTopics.java
@@ -1,6 +1,8 @@
 package com.greenapp.mailservice.kafka;
 
 
+import static com.greenapp.mailservice.config.KafkaConfigConstants.TOPIC_PREFIX;
+
 public interface MailTopics {
-    String MAIL_2FA_TOPIC = "2z2j7jw9-mail-auth2fa-account-created";
+    String MAIL_2FA_TOPIC = TOPIC_PREFIX + "mail-auth2fa-account-created";
 }
