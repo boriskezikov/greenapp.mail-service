bb7dbba2c46ac0210eace086dd07e06f69921b18
boriskezikov <boris200898@icloud.com>
2020-05-29 03:30:06 +0300

    kafka creds edit

--- src/main/java/com/greenapp/mailservice/config/KafkaConfigConstants.java
+++ src/main/java/com/greenapp/mailservice/config/KafkaConfigConstants.java
@@ -1,8 +1,8 @@
 package com.greenapp.mailservice.config;
 
 public interface KafkaConfigConstants {
-    String BOOTSTRAP = "moped-01.srvs.cloudkafka.com:9094,moped-02.srvs.cloudkafka.com:9094,moped-03.srvs.cloudkafka.com:9094";
-    String USERNAME = "uryu7l0c";
-    String PASSWORD = "rh2dpUrTHe7tGIuXZbmX2D4vkyjZZgUN";
+    String BOOTSTRAP = "rocket-01.srvs.cloudkafka.com:9094,rocket-02.srvs.cloudkafka.com:9094,rocket-03.srvs.cloudkafka.com:9094";
+    String USERNAME = "u4t0gea8";
+    String PASSWORD = "nXUyC5ThHzO8Nvx8fkwnQHGFA6cvz_IO";
     String TOPIC_PREFIX = USERNAME + "-";
 }
