b06dfc56202abaa355c5a7f749cf97aa73eb05c7
boriskezikov <boris200898@icloud.com>
2020-06-01 16:30:23 +0300

    Keep mail alive

--- src/main/java/com/greenapp/mailservice/StateAliveController.java
+++ src/main/java/com/greenapp/mailservice/StateAliveController.java
@@ -1,13 +1,29 @@
 package com.greenapp.mailservice;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class StateAliveController {
 
-    @GetMapping("/leave")
+    @GetMapping("/live")
     public void leave(){
         System.out.println("Leave request accepted.");
+    }
+
+    @Scheduled(cron = "* */15 * * * *")
+    public void keepGatewayAlivePlease() {
+
+        var restTemplate = new RestTemplate();
+        restTemplate.exchange(
+                "https://greenapp-gateway.herokuapp.com/live",
+                HttpMethod.GET,
+                null,
+                Void.class
+        );
+        System.out.println("Sent alive");
     }
 }
